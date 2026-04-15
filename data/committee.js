5820d54367c8256ece895eb1c2161e01fad05ac2
Benilton Carvalho <benilton@users.noreply.github.com>
2026-04-15 11:35:59 -0300

    atualizacao com comissao cientifica

--- data/committee.js
+++ data/committee.js
@@ -22,5 +22,21 @@ const COMMITTEE = [
       { nome: "Prof. Dr. Victor Freguglia Souza", instituicao: "UNICAMP"}
 
     ]
+  },
+    {
+    grupo: "⚙️ Comissão Científica",
+    membros: [
+      { nome:, instituicao: },
+      { nome: "Prof. Dr. Agatha Sacramento Rodrigues",  instituicao: "UFES"},
+      { nome: "Prof. Dr. Anderson Luiz Ara",   instituicao: "UFPR"},
+      { nome: "Prof. Dr. Daiane Aparecida Zuanetti",     instituicao: "UFSCar"},
+      { nome: "Profa. Dra. Fernanda Lang Schumacher",  instituicao: "The Ohio State University"},
+      { nome: "Profa. Dra. Idemauro Antonio Rodrigues de Lara", instituicao: "ESALQ (USP)"},
+      { nome: "Prof. Dr. Nancy Lopes Garcia", instituicao: "UNICAMP"},
+      { nome: "Profa. Dra. Rafael de Andrade Moral", instituicao: "Maynooth University"},
+      { nome: "Profa. Dra. Vera Lucia Damasceno Tomazella", instituicao: "UFSCar e UFPB"},
+      { nome: "Prof. Dr. Victor Hugo Lachos", instituicao: " University of Connecticut"}
+
+    ]
   }
 ];
